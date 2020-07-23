6d24963dbd77b39a568d7d8603dd1f9611ebddeb
Idan <idan.zonshein@powerhrg.com>
2020-07-23 10:24:27 -0400

    migrate

---
+++ golf-bag-backend/db/schema.rb
@@ -0,0 +1,41 @@
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# This file is the source Rails uses to define your schema when running `rails
+# db:schema:load`. When creating a new database, `rails db:schema:load` tends to
+# be faster and is potentially less error prone than running all of your
+# migrations from scratch. Old migrations may fail to apply correctly if those
+# migrations use external dependencies or application code.
+#
+# It's strongly recommended that you check this file into your version control system.
+
+ActiveRecord::Schema.define(version: 2020_07_22_191256) do
+
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
+  create_table "bags", force: :cascade do |t|
+    t.string "name"
+    t.string "color"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
+
+  create_table "clubs", force: :cascade do |t|
+    t.string "type"
+    t.string "loft"
+    t.string "brand"
+    t.string "model"
+    t.integer "price"
+    t.string "handedness"
+    t.string "lie"
+    t.string "image_source"
+    t.bigint "bag_id", null: false
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+    t.index ["bag_id"], name: "index_clubs_on_bag_id"
+  end
+
+  add_foreign_key "clubs", "bags"
+end
