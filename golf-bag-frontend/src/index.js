5f234481b233f5cb75199d2f6b6e78eb5248ef6a
Idan <idan.zonshein@powerhrg.com>
2020-07-28 08:07:42 -0400

    Add Html to forums

--- golf-bag-frontend/src/index.js
+++ golf-bag-frontend/src/index.js
@@ -1,42 +1,70 @@
 
 document.addEventListener('DOMContentLoaded', function() {
-  showAllBags()
+  grabAllBags()
+
 })
 
 
-const bagList = document.querySelector("#bag-list")
+function appendBags(bags) {
+  const bagList = document.querySelector("#bag-list")
+  for (const bag of bags) {
+    const bagEl = document.createElement("li")
+    bagEl.classList.add("list-group-item")
+    bagEl.id = `bag-id-${bag.id}`
 
-function appendBags(bag) {
-  const bagEl = document.createElement("li")
-  bagEl.classList.add("list-group-item")
-  bagEl.id = `bag-id-${bag.id}`
-  bagEl.innerHTML = `
-    <span>${bag.name}'s Bag</span>
-    <button id="delete-comment-${bag.id}"class="btn btn-danger btn-sm pull-right">Delete</button>
-  `
-  bagList.appendChild(bagEl)
+    bagEl.innerHTML = `
+      <span>${bag.name}'s Bag</span>
+      <button id="delete-bag-${bag.id}"class="btn btn-danger btn-sm pull-right">Delete</button>
+      <ol id="data-bag-id-${bag.id}"></ol>
+    `
+    bagList.appendChild(bagEl)
+    listenForDeleteButton(bag)
+    getClubsInBag(bag)
+  }
 
-  listenForDeleteButton(bag)
+  listenForAddClub()
+
 
 }
 
+function getClubsInBag(bag) {
+  fetch(`http://localhost:3000/bags/${bag.id}/clubs`).then(resp => resp.json()).then(function(clubs) {
+    appendClubs(clubs)
 
-function showAllBags() {
-  fetch("http://localhost:3000/bags").then(resp => resp.json()).then(function(bags) {
-    for (const bag of bags) {
-      appendBags(bag)
-    }
   })
 }
 
-const BagNameSave = document.querySelector("#new-club-form")
+function appendClubs(bag) {
+  const clubOl = document.querySelector(`#data-bag-id-${bag[0].bag_id}`)
+  for(const club of bag) {
+    const clubEl = document.createElement("li")
+    clubEl.classList.add("list-group-item")
+    clubEl.innerHTML = `
+    ${club.club_type}---${club.brand}---${club.model}---Loft: ${club.loft}---Hand: ${club.handedness}
+    `
+    clubOl.appendChild(clubEl)
+  }
+
+}
+
+
+
+function grabAllBags() {
+  fetch("http://localhost:3000/bags").then(resp => resp.json()).then(function(bags) {
+      appendBags(bags)
+      appendNameDropdown(bags)
+  })
+}
+
+const BagNameSave = document.querySelector("#new-bag-form")
 BagNameSave.addEventListener("submit", function(event) {
   event.preventDefault()
     createBag()
+
 })
 
 function listenForDeleteButton(bag) {
-  const delButton = document.getElementById(`delete-comment-${bag.id}`)
+  const delButton = document.getElementById(`delete-bag-${bag.id}`)
   delButton.addEventListener("click", function(event){
     deleteBagById(bag)
   })
@@ -73,6 +101,8 @@ function createBag() {
   }).then(function(bagName){
     appendBags(bagName)
 
+
+
     bagNameElem.value = ""
   }).catch(function(error){
     console.log(error.message)
@@ -81,6 +111,59 @@ function createBag() {
 
 
 
+function appendNameDropdown(bags) {
+  const nameList = document.getElementById("pick-bag-name")
+
+  nameList.innerHTML = ""
+  for(const bag of bags) {
+    const nameEl = document.createElement("option")
+    nameEl.id = `bag-id-${bag.id}`
+    nameEl.value = bag.id
+    nameEl.innerHTML = `
+    ${bag.name}
+    `
+    nameList.appendChild(nameEl)
+  }
+}
+
+function listenForAddClub() {
+  const addClubBtn = document.getElementById("submit-club")
+  addClubBtn.addEventListener("click", function(event){
+    addNewClubToBag()
+  })
+}
+
+function addNewClubToBag() {
+
+  const selectNameEl = document.querySelector("#pick-bag-name")
+  const clubTypeEl = document.querySelector("#add-club-type")
+  const clubBrandEl = document.querySelector("#add-brand-input")
+  const clubModelEl = document.querySelector("#add-model-input")
+  const clubLoftEl = document.querySelector("#add-loft-input")
 
 
-// const bagDiv = document.getElementById("bag-show")
+  fetch(`http://localhost:3000/bags/${selectNameEl.value}/clubs`, {
+    method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+        "Accept": "application/json"
+      },
+      body: JSON.stringify({
+        club_type: clubTypeEl.value,
+        brand: clubBrandEl.value,
+        model: clubModelEl.value,
+        loft: clubLoftEl.value
+      })
+  }).then(function(resp) {
+    return resp.json()
+  }).then(function(updatedBag){
+    appendClubs(updatedBag)
+
+    clubBrandEl.value = ""
+    clubModelEl.value = ""
+    clubLoftEl.value = ""
+  }).catch(function(error){
+    console.log(error.message)
+  })
+}
+
